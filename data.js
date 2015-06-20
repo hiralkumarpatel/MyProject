87c7b8e5db20d522ee987aa126ddab0f26cbe42a
hiralkumarpatel <hiral_2784@yahoo.com>
2015-06-20 10:40:40 +1000

    Added some files
    Added 3 files

---
+++ data.js
@@ -0,0 +1,16 @@
+var data = [
+{
+	'id': '1',
+	'firstName': 'Hiral',
+	'lastName': 'Patel',
+},
+{
+	'id': '2',
+	'firstName': 'Abhi',
+	'lastName': 'Patel',
+},
+{
+	'id': '3',
+	'firstName': 'Jig',
+	'lastName': 'Patel',
+}]
